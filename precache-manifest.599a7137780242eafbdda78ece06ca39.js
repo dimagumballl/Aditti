ba05fb8c94ae725d8554f1304cdbccac57a204be
unknown <dimagumball@gmail.com>
2020-02-19 20:40:54 +0200

    Updates

---
+++ precache-manifest.599a7137780242eafbdda78ece06ca39.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b634259ffeb472d8ae6747e117bc3b5",
+    "url": "/Aditti/index.html"
+  },
+  {
+    "revision": "52608b488c8004e539b2",
+    "url": "/Aditti/static/css/main.b34522bd.chunk.css"
+  },
+  {
+    "revision": "72cc02d33fe1d4f52c6d",
+    "url": "/Aditti/static/js/2.61146d6c.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/Aditti/static/js/2.61146d6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52608b488c8004e539b2",
+    "url": "/Aditti/static/js/main.536fd18d.chunk.js"
+  },
+  {
+    "revision": "3d788a3ffeac1dd05ad1",
+    "url": "/Aditti/static/js/runtime-main.c28f8df9.js"
+  },
+  {
+    "revision": "bde95fd4c7fcef652a3f6ee99170e09b",
+    "url": "/Aditti/static/media/facebook.bde95fd4.svg"
+  },
+  {
+    "revision": "1cc0cb4236b4d9fba6a91058f1d396fa",
+    "url": "/Aditti/static/media/instagram.1cc0cb42.svg"
+  },
+  {
+    "revision": "919175c7bec7fbb785706d83213e5723",
+    "url": "/Aditti/static/media/mc.919175c7.svg"
+  },
+  {
+    "revision": "8d16fc56f41706d3a7a7d87bfa79242b",
+    "url": "/Aditti/static/media/privat.8d16fc56.svg"
+  },
+  {
+    "revision": "e0473c0f89558403c2d73b48ef126050",
+    "url": "/Aditti/static/media/search.e0473c0f.png"
+  },
+  {
+    "revision": "191977d4d85d15c543c777cbeb19b328",
+    "url": "/Aditti/static/media/viber.191977d4.svg"
+  },
+  {
+    "revision": "d97b9761c885e4150f63e28387f224e9",
+    "url": "/Aditti/static/media/visa.d97b9761.svg"
+  },
+  {
+    "revision": "2a1a5f5e562e404469ecead79e9eaceb",
+    "url": "/Aditti/static/media/youtube.2a1a5f5e.svg"
+  }
+]);
